48d4fa5967de527a5ebfa8453550c2bf35147fd1
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-09-26 23:35:06 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,20 @@
                     <h1 class="text-5xl font-extrabold tracking-wider text-center text-gray-600">
                         {{ config('app.name') }}
                     </h1>
-
+                    <ul class="list-reset">
+                      <li class="inline px-4">
+                            <a href="https://tailwindcss.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Tailwind CSS</a>
+                        </li>  
+                        <li class="inline px-4">
+                            <a href="https://github.com/alpinejs/alpine" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Alpine.js</a>
+                        </li>
+                        <li class="inline px-4">
+                            <a href="https://laravel.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Laravel</a>
+                        </li>
+                        <li class="inline px-4">
+                            <a href="https://laravel-livewire.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Livewire</a>
+                        </li>
+                    </ul>
 
                 </div>
             </div>
