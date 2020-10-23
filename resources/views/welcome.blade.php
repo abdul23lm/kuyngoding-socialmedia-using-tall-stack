6ed76d881f4b173e3409f8ec7b8ad5bd925da9e9
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-10-23 23:42:06 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,9 @@
         <div class="flex items-center justify-center">
             <div class="flex flex-col justify-around">
                 <div class="space-y-6">
-
+                    <a href="{{ route('home') }}">
+                        <x-logo class="w-auto h-16 mx-auto text-indigo-600" />
+                    </a>
 
                     <h1 class="text-5xl font-extrabold tracking-wider text-center text-gray-600">
                         {{ config('app.name') }}
