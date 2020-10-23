994cf0a7a8751f2c94c28bdd03a81d61b34f0721
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-10-23 23:41:42 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,9 @@
                         <li class="inline px-4">
                             <a href="https://laravel.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Laravel</a>
                         </li>
-
+                        <li class="inline px-4">
+                            <a href="https://laravel-livewire.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Livewire</a>
+                        </li>
                     </ul>
 
                 </div>
