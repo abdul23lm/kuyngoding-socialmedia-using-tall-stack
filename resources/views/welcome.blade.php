49f8e10d7c47cd5601179bc34eb9cdce94495018
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-11-19 22:47:42 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     <div class="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
         <div class="absolute top-0 right-0 mt-4 mr-4">
+            @if (Route::has('login'))
+                <div class="space-x-4">
 
+                </div>
+            @endif
         </div>
 
         <div class="flex items-center justify-center">
